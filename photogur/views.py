3d7e55f9e70f9ff0cad90a2219aa9b73f81e3f48
christinegithub <christine13@gmail.com>
2019-03-01 15:30:36 -0500

    to do - comment count

--- photogur/views.py
+++ photogur/views.py
@@ -4,6 +4,7 @@ from photogur.models import Picture, Comment
 
 
 def picture_page(request):
-    context = {'pictures': Picture.objects.all(), 'comments': Comment.objects.count() }
+    context = {'pictures': Picture.objects.all(),
+    'comments': Comment.objects.filter(id = 2).count() }
     response = render(request, 'pictures.html', context)
     return HttpResponse(response)
