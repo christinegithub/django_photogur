b7a1e3f00530802380106ab15be16bdaec763047
christinegithub <christine13@gmail.com>
2019-03-11 13:30:51 -0400

    logout added

--- photogur/views.py
+++ photogur/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from photogur.models import Picture, Comment
 from photogur.forms import LoginForm
 
@@ -48,3 +48,7 @@ def login_view(request):
     context = {'form': form}
     response = render(request, 'login.html', context)
     return HttpResponse(response)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/pictures')
